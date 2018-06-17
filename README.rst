7d7f24cea4a18c11110eb1dd350eea5ebd4ee73d
Lyrositor <gagne.marc@gmail.com>
2018-06-17 17:16:58 +0200

    Import and display all data

---
+++ README.rst
@@ -0,0 +1,14 @@
+===========
+frangiclave
+===========
+
+*There are keys that open doors; then there are keys that destroy them.*
+
+**frangiclave** is an open-source Python tool for modding and documenting `Cultist Simulator <http://weatherfactory.biz/cultist-simulator/>`_ by Weather Factory.
+It is a web-based tool that can be set either to a read-only mode that only allows for browsing the documentation, or a full-on editing mode for modifying game data.
+
+**License:** |license|
+
+.. |license| image:: https://licensebuttons.net/p/zero/1.0/80x15.png
+   :height: 15
+   :width: 80
--- frangiclave/server.py
+++ frangiclave/server.py
@@ -6,8 +6,12 @@ from sqlalchemy.orm.exc import NoResultFound
 
 from frangiclave.compendium.base import get_session
 from frangiclave.compendium.deck import Deck
-from frangiclave.compendium.file import File, FileCategory
-from frangiclave.unity.importer import import_game_data
+from frangiclave.compendium.element import Element
+from frangiclave.compendium.file import File
+from frangiclave.compendium.legacy import Legacy
+from frangiclave.compendium.recipe import Recipe
+from frangiclave.compendium.verb import Verb
+from frangiclave.game.importer import import_game_data
 
 ROOT_DIR = abspath(dirname(__file__))
 STATIC_DIR = join(ROOT_DIR, 'static')
@@ -23,13 +27,17 @@ def index():
 
 @app.route('/load/')
 def load():
-    import_game_data(r'C:\Program Files (x86)\Steam\steamapps\common\Cultist Simulator\cultistsimulator_Data\StreamingAssets\content\core')
-    return ''
+    if app.config['READ_ONLY']:
+        abort(403)
+    import_game_data(app.config['GAME_DIRECTORY'])
+    return 'Game data loaded.'
 
 
 @app.route('/save/')
 def save():
-    pass
+    if app.config['READ_ONLY']:
+        abort(403)
+    return 'Game data saved.'
 
 
 @app.route('/file_add/', methods=['POST'])
@@ -57,6 +65,46 @@ def deck(deck_id: str):
         )
 
 
+@app.route('/element/<string:element_id>/')
+def element(element_id: str):
+    with get_session() as session:
+        return render_template(
+            'element.tpl.html',
+            element=Element.get_by_element_id(session, element_id),
+            show_elements=True
+        )
+
+
+@app.route('/legacy/<string:legacy_id>/')
+def legacy(legacy_id: str):
+    with get_session() as session:
+        return render_template(
+            'legacy.tpl.html',
+            legacy=Legacy.get_by_legacy_id(session, legacy_id),
+            show_legacies=True
+        )
+
+
+@app.route('/recipe/<string:recipe_id>/')
+def recipe(recipe_id: str):
+    with get_session() as session:
+        return render_template(
+            'recipe.tpl.html',
+            recipe=Recipe.get_by_recipe_id(session, recipe_id),
+            show_recipes=True
+        )
+
+
+@app.route('/verb/<string:verb_id>/')
+def verb(verb_id: str):
+    with get_session() as session:
+        return render_template(
+            'verb.tpl.html',
+            verb=Verb.get_by_verb_id(session, verb_id),
+            show_verbs=True
+        )
+
+
 @app.errorhandler(NoResultFound)
 def handle_invalid_usage(error):
     return page_not_found(error)
@@ -72,11 +120,35 @@ def add_global_variables():
     with get_session() as session:
         file_list = session.query(File).order_by(File.name).all()
         files = defaultdict(lambda: OrderedDict())
-        decks = session.query(Deck.deck_id, Deck.file_id).order_by(
-            Deck.deck_id).all()
+        decks = (
+            session.query(Deck.deck_id, Deck.file_id)
+            .order_by(Deck.deck_id)
+            .all()
+        )
+        elements = (
+            session.query(Element.element_id, Element.file_id)
+            .order_by(Element.element_id)
+            .all()
+        )
+        legacies = (
+            session.query(Legacy.legacy_id, Legacy.file_id)
+            .order_by(Legacy.legacy_id)
+            .all()
+        )
+        recipes = (
+            session.query(Recipe.recipe_id, Recipe.file_id)
+            .order_by(Recipe.recipe_id)
+            .all()
+        )
+        verbs = (
+            session.query(Verb.verb_id, Verb.file_id)
+            .order_by(Verb.verb_id)
+            .all()
+        )
+        items = decks + elements + legacies + recipes + verbs
         for file in file_list:
             files[file.category.value][file] = [
-                deck_id for deck_id, file_id in decks if file_id == file.id
+                item_id for item_id, file_id in items if file_id == file.id
             ]
         session.expunge_all()
     return dict(
@@ -84,6 +156,7 @@ def add_global_variables():
         read_only=app.config['READ_ONLY'],
         decks_open=False,
         elements_open=False,
+        legacies_open=False,
         recipes_open=False,
         verbs_open=False
     )
